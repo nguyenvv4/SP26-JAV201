f217cc0769f4d5ed8f1623f6ef3a17a8754dea7d
nguyenvv4 <nguyenvv4@fpt.edu.vn>
2026-01-10 13:51:34 +0700

    update

--- src/main/java/com/example/jav201/repository/UserRepository.java
+++ src/main/java/com/example/jav201/repository/UserRepository.java
@@ -6,20 +6,39 @@ import jakarta.persistence.TypedQuery;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserRepository {
 
     public List<User> getList() {
         Session session = HibernateUtils.getFACTORY().openSession();
-        String jpql ="Select u from User u";
+        String jpql = "Select u from User u";
         TypedQuery<User> query = session.createQuery(jpql);
         return query.getResultList();
     }
 
+    public List<User> page() {
+        int page = 1;     // bắt đầu từ 1
+        int size = 3;
+        List<User> data = new ArrayList<>();
+        try (Session session = HibernateUtils.getFACTORY().openSession()) {
+            Query<User> q = session.createQuery(
+                    "SELECT u FROM User u",
+                    User.class
+            );
+            q.setFirstResult((page - 1) * size);
+            q.setMaxResults(size);
+            data = q.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
     public static void main(String[] args) {
         UserRepository userRepository = new UserRepository();
-        List<User> userList = userRepository.getList();
+        List<User> userList = userRepository.page();
         for (User user : userList) {
             System.out.println(user);
         }
